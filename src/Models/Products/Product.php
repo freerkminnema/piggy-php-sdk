681e1752f0e2f3c11eb7cc982e7ba347e5213a09
Lars Wolters <lars.wolters@piggy.nl>
2025-07-10 11:35:18 +0200

    wip

--- src/Models/Products/Product.php
+++ src/Models/Products/Product.php
@@ -33,20 +33,27 @@ class Product
     protected $description;
 
     /**
+     * @var array|null
+     */
+    protected $categories;
+
+    /**
      * @var string
      */
-    const resourceUri = '/api/v3/oauth/clients/external-products';
+    const resourceUri = '/api/v3/oauth/clients/products';
 
     public function __construct(
         string $uuid,
         string $externalIdentifier,
         string $name,
-        ?string $description
+        ?string $description,
+        ?array $categories
     ) {
         $this->uuid = $uuid;
         $this->externalIdentifier = $externalIdentifier;
         $this->name = $name;
         $this->description = $description;
+        $this->categories = $categories;
     }
 
     public function getUuid(): string
@@ -64,67 +71,13 @@ class Product
         return $this->name;
     }
 
-    public function getDescription(): string
+    public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    /**
-     * @param  mixed[]  $params
-     * @return Product[]
-     *
-     * @throws MaintenanceModeException|GuzzleException|PiggyRequestException
-     */
-    public static function list(array $params = []): array
+    public function getCategories(): ?array
     {
-        $response = ApiClient::get(self::resourceUri, $params);
-
-        return ProductsMapper::map((array) $response->getData());
-    }
-
-    /**
-     * @param  mixed[]  $body
-     *
-     * @throws MaintenanceModeException|GuzzleException|PiggyRequestException
-     */
-    public static function create(array $body): Product
-    {
-        $response = ApiClient::post(self::resourceUri, $body);
-
-        return ProductMapper::map($response->getData());
-    }
-
-    /**
-     * @param  mixed[]  $params
-     *
-     * @throws MaintenanceModeException|GuzzleException|PiggyRequestException
-     */
-    public static function get(string $perkUuid, array $params = []): Product
-    {
-        $response = ApiClient::get(self::resourceUri."/$perkUuid", $params);
-
-        return ProductMapper::map($response->getData());
-    }
-
-    /**
-     * @param  mixed[]  $body
-     *
-     * @throws MaintenanceModeException|GuzzleException|PiggyRequestException
-     */
-    public static function update(string $perkUuid, array $body): Product
-    {
-        $response = ApiClient::put(self::resourceUri."/$perkUuid", $body);
-
-        return ProductMapper::map($response->getData());
-    }
-
-    /**
-     * @param  mixed[]  $params
-     *
-     * @throws MaintenanceModeException|GuzzleException|PiggyRequestException
-     */
-    public static function delete(string $perkUuid, array $params = []): Response
-    {
-        return ApiClient::delete(self::resourceUri."/$perkUuid", $params);
+        return $this->categories;
     }
 }
